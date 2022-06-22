122731a08a53177931cbc071db5fcea359d590dc
taiyangAh <wulian404@gmail.com>
2022-06-22 23:40:23 +0800

    Authenticate users

--- src/App.js
+++ src/App.js
@@ -6,26 +6,38 @@ import Sidebar from "./components/Sidebar";
 import HomeAuth from "./pages/HomeAuth";
 import Rightbar from "./components/Rightbar";
 import "./App.css";
+import { ConnectButton } from "web3uikit";
+import logo from "./images/medium.png";
+import { useMoralis } from "react-moralis";
 
 const App = () => {
+  const { isAuthenticated } = useMoralis();
+
   return (
     <>
-      <div className="App">
-        <div className="sideBar">
-          <Sidebar />
+      {isAuthenticated ? (
+        <div className="App">
+          <div className="sideBar">
+            <Sidebar />
+          </div>
+          <div className="mainWindow">
+            <Routes>
+              <Route path="/" element={<HomeAuth />} />
+              <Route path="/newStory" element={<NewStory />} />
+              <Route path="/myBlogs" element={<MyBlogs />} />
+              <Route path="/blog/:url" element={<Blog />} />
+            </Routes>
+          </div>
+          <div className="rightBar">
+            <Rightbar />
+          </div>
         </div>
-        <div className="mainWindow">
-          <Routes>
-            <Route path="/" element={<HomeAuth />} />
-            <Route path="/newStory" element={<NewStory />} />
-            <Route path="/myBlogs" element={<MyBlogs />} />
-            <Route path="/blog/:url" element={<Blog />} />
-          </Routes>
+      ) : (
+        <div className="unAuth">
+          <img src={logo} alt="logo" height="200px" />
+          <ConnectButton />
         </div>
-        <div className="rightBar">
-          <Rightbar />
-        </div>
-      </div>
+      )}
     </>
   );
 };
